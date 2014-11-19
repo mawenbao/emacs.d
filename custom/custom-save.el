ce922d0d39389249ceacb4797cd6a9fd323aacf6
wilbur ma <mawenbao@hotmail.com>
2014-11-19 16:57:22 +0800

    use zenburn theme

--- custom/custom-save.el
+++ custom/custom-save.el
@@ -3,8 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(bmkp-last-as-first-bookmark-file "~/.emacs.d/save.d/bookmark_save.el")
  '(column-number-mode t)
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes (quote ("9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" default)))
  '(ede-project-directories (quote ("/home/wilbur/projects/cpp11-examples")))
  '(initial-frame-alist (quote ((fullscreen . maximized))))
  '(save-place t nil (saveplace))
