fcaad155b0d260e36672122603de25f1dc9b0051
wilbur ma <mawenbao@hotmail.com>
2014-11-19 17:09:56 +0800

    try to resolve the helm-show-kill-ring problem with session.el

--- custom/setup-packages.el
+++ custom/setup-packages.el
@@ -64,5 +64,6 @@
 (require 'session)
 (add-hook 'after-init-hook 'session-initialize)
 (add-to-list 'session-globals-exclude 'org-mark-ring)
+(setq session-save-print-spec '(t nil 40000))
 
 ;; misc package settings
