145fbe1858e41e1dc1f5a52a130cb6aa741da587
mawenbao <mwenbao@gmail.com>
2015-10-01 10:51:13 +0800

    update cpp settings

--- custom/setup-prog.el
+++ custom/setup-prog.el
@@ -9,7 +9,6 @@ n
  ;; gdb-show-main t
  )
 
-(add-hook 'c-mode-common-hook 'hs-minor-mode)
 (add-hook 'prog-mode-hook (lambda () (interactive) (setq show-trailing-whitespace 1)))
 
 (setq-default indent-tabs-mode nil)
@@ -24,17 +23,17 @@ n
 (add-hook 'python-mode-hook 'jedi:setup)
 ;;(setq jedi:complete-on-dot t)
 
+;; open .h files with c++-mode
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+(add-hook 'c-mode-common-hook 'hs-minor-mode)
 ;; c/cpp complete
 (add-hook 'c-mode-common-hook 'company-mode)
-
 ;; c/cpp style
-(add-hook 'c-mode-common-hook 'my-custom-cpp-style-hook)
-
 (c-add-style "my-custom-style"
              '("stroustrup"
                (c-offsets-alist . ((innamespace . [0])))
                (c-basic-offset . 4)))
-
 (defun my-custom-cpp-style-hook()
   (setq c-default-style "my-custom-style")
   (c-set-style "my-custom-style"))
+(add-hook 'c-mode-common-hook 'my-custom-cpp-style-hook)
