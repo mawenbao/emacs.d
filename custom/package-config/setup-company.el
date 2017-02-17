6a3136d8bf79c2d7c78333cd1ffe5e69b4907066
mawenbao <mwenbao@gmail.com>
2017-02-17 11:09:49 +0800

    update company mode config

--- custom/package-config/setup-company.el
+++ custom/package-config/setup-company.el
@@ -2,17 +2,18 @@
 (provide 'setup-company)
 (require 'company)
 
+(setq company-dabbrev-downcase nil)
 (add-hook 'after-init-hook 'global-company-mode)
 
 (setq company-backends
       '((company-files          ; files & directory
          company-keywords       ; keywords
          company-capf
-         company-abbrev
-         company-dabbrev
+         company-dabbrev-code
          company-yasnippet
          company-semantic
          company-cmake
          company-gtags
+         company-abbrev
          )
         ))
